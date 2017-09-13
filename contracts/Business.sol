98f9639b3de4a12e82a82684a05d04f251b74096
Anton Bukov <k06aaa@gmail.com>
2017-09-13 19:04:39 +0300

    Add Offer contract

--- contracts/Business.sol
+++ contracts/Business.sol
@@ -4,15 +4,48 @@ import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 import "./helpers/AvoidRecursiveCall.sol";
 import "./Registrator.sol";
 import "./Registered.sol";
+import "./Offer.sol";
 
 
 contract Business is Registered, Ownable, AvoidRecursiveCall {
 
+    mapping(address => bool) public allOffers;
+    mapping(address => bool) public allCoinSales;
+    Offer[] public offers;
     CoinSale[] public coinSales;
     
     event CoinSaleAdded(address wallet);
 
     function Business(Registrator registratorArg) Registered(registratorArg) {
+    }
+
+    function offersCount() constant returns(uint) {
+        return offers.length;
+    }    
+
+    function createOffer() onlyOwner returns(Offer) {
+        Offer offer = new Offer(registrator, this);
+        offer.transferOwnership(msg.sender);
+        allOffers[offer] = true;
+        offers.push(offer);
+        return offer;
+    }
+
+    function deleteOffer(Offer offer) onlyOwner {
+        require(offer.owner() == msg.sender);
+        require(allOffers[offer]);
+        delete allOffers[offer];
+
+        for (uint i = 0; i < offers.length; i++) {
+            if (offers[i] == offer) {
+                delete offers[i];
+                offers[i] = offers[offers.length - 1];
+                offers.length -= 1;
+                return;
+            }
+        }
+
+        revert();
     }
 
     function coinSalesCount() constant returns(uint) {
@@ -23,6 +56,7 @@ contract Business is Registered, Ownable, AvoidRecursiveCall {
         for (uint i = 0; i < coinSales.length; i++) {
             require(coinSales[i] != coinSale);
         }
+        allCoinSales[coinSale] = true;
         coinSales.push(coinSale);
         CoinSaleAdded(coinSale);
     }
--- contracts/Offer.sol
+++ contracts/Offer.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.4.2;
 
 import "zeppelin-solidity/contracts/ownership/Ownable.sol";
+import "zeppelin-solidity/contracts/token/ERC20.sol";
 import "./helpers/AvoidRecursiveCall.sol";
 import "./Registrator.sol";
 import "./Registered.sol";
@@ -13,6 +14,17 @@ contract Offer is Registered, Ownable, AvoidRecursiveCall {
 
     function Offer(Registrator registratorArg, Business businessArg) Registered(registratorArg) {
         business = businessArg;
+    }
+
+    function show(Customer customer) onlyRegistrator {
+        require(registrator == customer.registrator());
+        require(registrator == business.registrator());
+        require(registrator.allCustomers(customer));
+        require(registrator.allBusinesses(business));
+        
+        //TODO: Send ERC20 coins to customer
+        //ERC20 token = ERC20(0x348438257643856483256475683425);
+        //token.transfer(customer.owner());
     }
 
 }
